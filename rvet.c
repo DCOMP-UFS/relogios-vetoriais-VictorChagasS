b7dc4e6eb3540cfa66bcfda03f48f61cf12732a4
Victor Chagas <42676345+VictorChagasS@users.noreply.github.com>
2024-02-08 22:43:19 -0300

    Update rvet.c

--- rvet.c
+++ rvet.c
@@ -1,14 +1,3 @@
-/**
- * Código base (incompleto) para implementação de relógios vetoriais.
- * Meta: implementar a interação entre três processos ilustrada na figura
- * da URL: 
- * 
- * https://people.cs.rutgers.edu/~pxk/417/notes/images/clocks-vector.png
- * 
- * Compilação: mpicc -o rvet rvet.c
- * Execução:   mpiexec -n 3 ./rvet
- */
- 
 //Marcus Ryller Fonseca Amado dos Santos
 //Guilherme Argolo Queiroz de Freitas
 //Victor Santos Chagas
@@ -17,73 +6,73 @@
 #include <string.h>  
 #include <mpi.h> 
 
-
 typedef struct Clock { 
   int p[3];
 } Clock;
 
 void Event(int pid, Clock *clock) {
   clock->p[pid]++;   
-  printf("Process: %d, Clock: (%d, %d, %d)\n", pid, clock->p[0], clock->p[1], clock->p[2]);
 }
 
-void Send(int dest_rank, Clock *clock) {
-  int my_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+void Send(int pidEmissor, int pidRecep, Clock *clock) {
+ 
+  clock->p[pidEmissor]++;
 
-  clock->p[my_rank]++;
-
-  // Vetor auxiliar
+ 
   int clockAux[3];
   for (int i = 0; i < 3; i++) clockAux[i] = clock->p[i];
 
-  // Utilizar MPI
-  MPI_Send(clockAux, 3, MPI_INT, dest_rank, my_rank, MPI_COMM_WORLD);
-
-  // Exibir alterações
-  printf("Process: %d, Clock: (%d, %d, %d)\n", my_rank, clock->p[0], clock->p[1], clock->p[2]);
+  MPI_Send(clockAux, 3, MPI_INT, pidRecep, pidEmissor, MPI_COMM_WORLD);
 }
 
-void Receive(Clock *clock) {
-  int my_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-  clock->p[my_rank]++;
+void Receive(int pidEmissor, int pidRecep, Clock *clockR) {
+ 
+  clockR->p[pidRecep]++;
 
 
   int clockE[3];
-  MPI_Recv(clockE, 3, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  MPI_Recv(clockE, 3, MPI_INT, pidEmissor, pidEmissor, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
+  
   for (int i = 0; i < 3; i++) {
-    if (clock->p[i] < clockE[i]) clock->p[i] = clockE[i];
+    if (clockR->p[i] < clockE[i]) clockR->p[i] = clockE[i];
   }
 
-  printf("Process: %d, Clock: (%d, %d, %d)\n", my_rank, clock->p[0], clock->p[1], clock->p[2]);
+ 
 }
 
 // Representa o processo de rank 0
 void process0() {
   Clock clock = {{0,0,0}};
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
   
   // 1, 0, 0
   Event(0, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
   
   // 2, 0, 0
-  Send(1, &clock);
+  Send(0, 1, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 
   // 3, 1, 0
-  Receive(&clock);
+  Receive(1, 0, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
   
-  // (4, 1, 0)
-  Send(2, &clock);
+  // 4, 1, 0
+  Send(0, 2, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
   
   // 5, 1, 2
-  Receive(&clock);
+  Receive(2, 0, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
   
   // 6, 1, 2
-  Send(1, &clock);
+  Send(0, 1, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
   
   // 7, 1, 2
   Event(0, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 }
 
 // Representa o processo de rank 1
@@ -92,13 +81,16 @@ void process1() {
   printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
   
   // 0, 1, 0
-  Send(0, &clock);
+  Send(1, 0, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 
   // 2, 2, 0
-  Receive(&clock);
+  Receive(0, 1, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 
   // 6, 3, 2
-  Receive(&clock);
+  Receive(0, 1, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 }
 
 // Representa o processo de rank 2
@@ -108,12 +100,15 @@ void process2() {
   
   // 0, 0, 1
   Event(2, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 
   // 0, 0, 2
-  Send(0, &clock);
+  Send(2, 0, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 
   // 4, 1, 3
-  Receive(&clock);
+  Receive(0, 2, &clock);
+  printf("Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
 }
 
 int main(void) {
@@ -138,4 +133,4 @@ int main(void) {
   MPI_Finalize(); 
 
   return 0;
-}
+}  /* main */
